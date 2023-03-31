61e56303e1497cffe88e0d091224cd1d4a6cf755
Ras Fincher <ras@rasfincher.com>
2023-03-31 11:38:41 -0400

    Removed signedout/signedin

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from "@remix-run/react";
-import { SignedIn, SignedOut } from "@clerk/remix";
 
 export default function Index() {
   return (
@@ -9,35 +8,28 @@ export default function Index() {
           <h1 className="text-shadow-lg text-center text-8xl font-bold uppercase text-slate-50">
             Smooth Jazz Stack
           </h1>
-          <SignedOut>
-            <p className="text-shadow-lg mt-6 text-center text-xl font-bold text-slate-100">
-              Minor friction, major performance, diminished complexity,
-              augmented productivity
-            </p>
-            <div className="mt-6 flex flex-row items-center justify-center space-x-4">
-              <Link
-                to="/sign-in"
-                className="inline-block rounded bg-white px-7 py-3 text-center text-lg font-medium leading-snug text-blue-600 shadow-md transition duration-150 ease-in-out hover:bg-gray-100 hover:shadow-lg focus:bg-gray-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-              >
-                Sign in
-              </Link>
-              <Link
-                to="/sign-up"
-                className="inline-block rounded bg-blue-600 px-7 py-3 text-center text-lg font-medium leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-              >
-                Sign up
-              </Link>
-            </div>
+          <p className="text-shadow-lg mt-6 text-center text-xl font-bold text-slate-100">
+            Minor friction, major performance, diminished complexity, augmented
+            productivity
+          </p>
+          <div className="mt-6 flex flex-row items-center justify-center space-x-4">
+            <Link
+              to="/sign-in"
+              className="inline-block rounded bg-white px-7 py-3 text-center text-lg font-medium leading-snug text-blue-600 shadow-md transition duration-150 ease-in-out hover:bg-gray-100 hover:shadow-lg focus:bg-gray-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+            >
+              Sign in
+            </Link>
+            <Link
+              to="/sign-up"
+              className="inline-block rounded bg-blue-600 px-7 py-3 text-center text-lg font-medium leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+            >
+              Sign up
+            </Link>
+          </div>
 
-            <p className="text-shadow-lg mt-6 text-center text-lg font-bold text-slate-100">
-              Built with Remix, Tailwind, and TypeScript. Deployed on Vercel.
-            </p>
-          </SignedOut>
-          <SignedIn>
-            <p className="text-shadow-lg mt-6 text-center text-xl font-bold text-slate-100">
-              I dig it. You dig it. We dig it.
-            </p>
-          </SignedIn>
+          <p className="text-shadow-lg mt-6 text-center text-lg font-bold text-slate-100">
+            Built with Remix, Tailwind, and TypeScript. Deployed on Vercel.
+          </p>
         </div>
       </div>
     </main>
