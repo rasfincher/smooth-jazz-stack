ad3d94d6936b1f8f4b67b148956953390e9ac066
Ras Fincher <ras@rasfincher.com>
2023-03-31 12:19:41 -0400

    Test

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,7 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
-import { ClerkApp } from "@clerk/remix";
+import { ClerkApp, ClerkCatchBoundary } from "@clerk/remix";
 
 import {
   Links,
@@ -21,8 +21,9 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const loader: LoaderFunction = (args) =>
-  rootAuthLoader(args, { loadUser: true });
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+
+export const CatchBoundary = ClerkCatchBoundary();
 
 function App() {
   return (
