33cc291239dbc59494ac1668b28a3b149330e05a
Ras Fincher <ras@rasfincher.com>
2023-03-31 09:56:50 -0400

    Ready to deploy

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="p-10">
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,99 +2,16 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
-    // center the content for a landing page using tailwindcss
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl backdrop-blur-sm sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <img
-                className="h-full w-full object-cover grayscale transition hover:grayscale-0 focus:grayscale-0"
-                src="../images/jazz.jpg"
-                alt="Jazz club neon sign"
-              />
-            </div>
-            <div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
-              <h1 className="text-center text-4xl font-extrabold tracking-tight drop-shadow-sm sm:text-5xl lg:text-8xl">
-                <span className="block uppercase text-yellow-500">
-                  Smooth Jazz Stack
-                </span>
-              </h1>
-
-              <p className="mx-auto mt-6 max-w-lg text-center text-4xl font-extrabold text-yellow-400 sm:max-w-3xl">
-                Minor friction, major speed, augmented productivity, diminished
-                stress
-              </p>
-
-              <a href="https://remix.run">
-                <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt="Remix"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
-          <div className="mt-6 flex flex-wrap justify-center gap-8">
-            {[
-              {
-                src: "https://raw.githubusercontent.com/clerkinc/remix-bossa-nova-stack/faf7080fad078604f884817e07a8115b7fa13c86/public/images/vercel.svg",
-                alt: "Vercel",
-                href: "https://vercel.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764484-ad64a21a-d7fb-47e3-8669-ec046da20c1f.svg",
-                alt: "Prisma",
-                href: "https://prisma.io",
-              },
-              {
-                src: "https://raw.githubusercontent.com/clerkinc/remix-bossa-nova-stack/faf7080fad078604f884817e07a8115b7fa13c86/public/images/supabase.svg",
-                alt: "Supabase",
-                href: "https://supabase.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
-                alt: "Tailwind",
-                href: "https://tailwindcss.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
-                alt: "Cypress",
-                href: "https://www.cypress.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772447-00fccdce-9d12-46a3-8bb4-fac612cdc949.svg",
-                alt: "Vitest",
-                href: "https://vitest.dev",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
-                alt: "Prettier",
-                href: "https://prettier.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
-                alt: "ESLint",
-                href: "https://eslint.org",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
-                alt: "TypeScript",
-                href: "https://typescriptlang.org",
-              },
-            ].map((img) => (
-              <a
-                key={img.href}
-                href={img.href}
-                className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-              >
-                <img alt={img.alt} src={img.src} className="object-contain" />
-              </a>
-            ))}
-          </div>
+    <main className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-[url('/images/jazz.jpg')] bg-cover">
+      <div className="flex h-full w-full flex-col items-center justify-center backdrop-blur-sm">
+        <div className="pb-10">
+          <h1 className="text-shadow-lg text-center text-8xl font-bold uppercase text-slate-50">
+            Smooth Jazz Stack
+          </h1>
+          <p className="text-shadow-lg mt-6 text-center text-xl font-bold text-slate-100">
+            Minor friction, major performance, diminished complexity, augmented
+            productivity
+          </p>
         </div>
       </div>
     </main>
